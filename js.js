0eec19fc9532c07481edf5420e52c3542b762391
Matthew Crane <matt.crane@mac.com>
2024-11-26 12:35:41 +0000

    shuffling files

--- js.js
+++
@@ -1,9 +0,0 @@
-function ecosiaEnable() {
-	document.getElementById("ecosia").style.display = "inline";
-	document.getElementById("google").style.display = "none";
-	}
-        
-function googleEnable() {
-	document.getElementById("google").style.display = "none";
-	document.getElementById("ecosia").style.display = "inline";
-	}
