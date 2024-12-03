328c4070e6c5f32f529e8d1549d5430395cb1e3a
Matthew Crane <matt.crane@mac.com>
2024-12-03 11:35:46 +0000

    side menu styling and function

--- Portfolio/javas.js
+++ Portfolio/javas.js
@@ -13,10 +13,14 @@ function typeWriter() {
 	setTimeout(typeWriter, 350);
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "265px";
+	document.getElementById("menubutton").style.width = "265px";
+	document.getElementById("menubutton").style.textAlign = "right";
   }
   
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("menubutton").style.width = "70px";
+	document.getElementById("menubutton").style.textAlign = "left";
   }
