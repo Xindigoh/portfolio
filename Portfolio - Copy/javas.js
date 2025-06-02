64aa59df60151cf473edf7fb50a9bcc2355779e6
Matthew Crane <matt.crane@mac.com>
2025-06-02 14:04:28 +0100

    Update javas.js

--- Portfolio - Copy/javas.js
+++ Portfolio - Copy/javas.js
@@ -22,6 +22,9 @@ function openNav() {
 	document.getElementById("mySidenav").style.transition = "0s"
   }
   
+/*/<button onclick="setTimeout(myFunction, 3000);">Try it</button>/*/
+
+  
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0px";
