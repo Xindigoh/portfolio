c3be4540f1a865a3445e97446246c63f49dcdc4d
Matthew Crane <matt.crane@mac.com>
2025-06-04 15:24:32 +0100

    rewrites, changes

--- Portfolio - Copy/javas.js
+++ Portfolio - Copy/javas.js
@@ -4,7 +4,7 @@ var speed = 80;
 
 function typeWriter() {
 	if (i < txt.length) {
-		document.getElementById("maintitle").innerHTML += txt.charAt(i);
+		document.getElementById("mainTitle").innerHTML += txt.charAt(i);
 		i++;
 		setTimeout(typeWriter, speed);
 		}
@@ -19,12 +19,11 @@ function openNav() {
 	document.getElementById("menubutton").style.width = "265px";
 	document.getElementById("menubutton").style.textAlign = "right";
 	document.getElementById("menubutton").style.color = "#333333";
-	document.getElementById("mySidenav").style.transition = "0s"
+	/*/document.getElementById("mySidenav").style.transition = "0s"/*/
   }
   
 /*/<button onclick="setTimeout(myFunction, 3000);">Try it</button>/*/
 
-  
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0px";
@@ -33,5 +32,5 @@ function openNav() {
 	document.getElementById("menubutton").style.width = "70px";
 	document.getElementById("menubutton").style.textAlign = "left";
 	document.getElementById("menubutton").style.color = "#f2f2f2";
-	document.getElementById("mySidenav").style.transition = "0.2s"
+	/*/document.getElementById("mySidenav").style.transition = "0.2s"/*/
   }
