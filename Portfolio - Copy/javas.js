47883acedf5e2ed72ff08b7bb160e02d4561fe3c
Matthew Crane <matt.crane@mac.com>
2025-06-04 15:48:47 +0100

    nav changes

--- Portfolio - Copy/javas.js
+++ Portfolio - Copy/javas.js
@@ -12,25 +12,6 @@ function typeWriter() {
 		
 	setTimeout(typeWriter, 350);
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "265px";
-	document.getElementById("navitems").style.width = "200px";
-	document.getElementById("navclosebar").style.width = "265px";
-	document.getElementById("menubutton").style.width = "265px";
-	document.getElementById("menubutton").style.textAlign = "right";
-	document.getElementById("menubutton").style.color = "#333333";
-	/*/document.getElementById("mySidenav").style.transition = "0s"/*/
-  }
-  
-/*/<button onclick="setTimeout(myFunction, 3000);">Try it</button>/*/
-
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0px";
-	document.getElementById("navitems").style.width = "0px";
-	document.getElementById("navclosebar").style.width = "0px";
-	document.getElementById("menubutton").style.width = "70px";
-	document.getElementById("menubutton").style.textAlign = "left";
-	document.getElementById("menubutton").style.color = "#f2f2f2";
-	/*/document.getElementById("mySidenav").style.transition = "0.2s"/*/
-  }
+function test() {
+	document.getElementById("test").style.width = "200px"
+}
